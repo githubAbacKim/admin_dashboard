bfd1ef790aee8077156de4a7dc67821c6be1c31b
ezmichaely <76212358+ezmichaely@users.noreply.github.com>
2022-12-20 18:46:26 +0800

    Update Fair Page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,3 +2,24 @@
 $(document).ready(function () {
     $('table').DataTable();
 });
+
+/** SELECT 2 */
+$(document).ready(function () {
+    $('#selectAppartment').select2( {
+        theme: 'bootstrap-5',
+        width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+        placeholder: $(this).data('placeholder'),
+    });
+});
+
+// VANILLA JS DATEPICKER
+document.addEventListener('DOMContentLoaded', function () {
+    const elem = document.getElementById('foo');
+    const rangepicker = new DateRangePicker(elem, {
+        orientation: "bottom left",
+        buttonClass: 'btn',
+        autoclose: false
+    });
+});
+
+
